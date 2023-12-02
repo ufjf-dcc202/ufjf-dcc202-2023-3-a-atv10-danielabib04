e349b06fef290838cba7de0f330ddc407c402ec4
danielabib04 <danielabib@outlook.com>
2023-12-01 22:13:13 -0300

    mudanças nos arquivos js

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+import { getJoao, getMaria, setJoao, setMaria } from "./joaoEMaria.js";
+
 // main.js
 console.log('módulo main');
+
+console.log('Joao tem ', getJoao(), ' maçãs!');
+console.log('Maria tem ', getMaria(), ' maçãs!');
+
